66f78442365a062398a493a7e6f875c74d2f29c3
mourthag <julispittel@gmail.com>
2014-03-13 20:09:23 +0100

    Added room spawning method
    Signed-off-by: mourthag <julispittel@gmail.com>

--- Dungeon.java
+++ Dungeon.java
@@ -21,7 +21,6 @@ public class Dungeon extends World
     public static final int tileWidth = frameWidth/tileCountX;
     public static final int tileHeight = frameHeight/tileCountY;
 
-
     /**
      * Constructor for objects of class Dungeon.
      * 
@@ -34,17 +33,87 @@ public class Dungeon extends World
         GreenfootImage background = getBackground();
         background.setColor(Color.BLACK);
         background.fill();
-        
+
         //set act order
         //player needs to be last because of the way we process visbility
         setActOrder(Tile.class, Player.class);
-        
+
         //tmp playground
         //addObject(new Wall(), 45,45);
         addObject(new Tile(), 70,45);
         addObject(new Player(), 70, 70);
+
+        spawnRoom( 100,100,10,10);
     }
 
+    /*
+     * spawns a rectangle room at postition x,y 
+     * its size is declared in Tiles
+     */
+    public void spawnRoom(int x, int y, int width, int height)
+    {
+        //coordinates for the opposite start
+        int opX = x + (width - 1) * tileWidth;
+        int opY = y + (height - 1) * tileHeight ;
+        
+        //inner coordinates for the floor
+        int innerWidth = width - 2;
+        int innerHeight = height - 2;
+        int innerX = x + tileWidth;
+        int innerY = y + tileHeight;
+        
+        //outer Boarders
+        createWallLine( x, y, true, width);                             //top
+        createWallLine( x, y + tileHeight, false, height - 1);          //left
+        createWallLine( innerX, opY, true, width - 1);                  //bottom
+        createWallLine( opX, innerY, false, height - 2);                 //right
+        
+        //Fill with Floor
+        for(int k = 0; k<innerHeight; k++)
+        {
+            createFloorLine(innerX , innerY + k*tileWidth, true, innerWidth);
+        }
+    }
 
-    
+    /*
+     * Creates a Line of Walls
+     */
+    public void createWallLine(int startX, int startY, boolean horizontal, int length)
+    {
+        if (horizontal)
+        {
+            for(int i=0; i<length; i++)
+            {
+                addObject(new Wall(), startX + i*tileWidth, startY);
+            }
+        }
+        else
+        {
+            for(int j=0; j<length; j++)
+            {
+                addObject(new Wall(), startX, startY + j*tileHeight);
+            }
+        }
+    }
+
+    /*
+     * Creates a Line of Floor
+     */
+    public void createFloorLine(int startX, int startY, boolean horizontal, int length)
+    {
+        if (horizontal)
+        {
+            for(int i=0; i<length; i++)
+            {
+                addObject(new Floor(), startX + i*tileWidth, startY);
+            }
+        }
+        else
+        {
+            for(int j=0; j<length; j++)
+            {
+                addObject(new Floor(), startX, startY + j*tileHeight);
+            }
+        }
+    }
 }
