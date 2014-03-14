90de2e4b33e30b0e0e18186f19e80a5002d0c0c6
mourthag <julispittel@gmail.com>
2014-03-14 16:22:54 +0100

    +added implemented an InpuDialog for width and height of the world -improved roomspawning

--- Dungeon.java
+++ Dungeon.java
@@ -1,6 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.awt.Color;
-import java.util.List
+import javax.swing.JOptionPane;
+import java.util.List;
 ;
 
 /**
@@ -8,14 +9,13 @@ import java.util.List
  * The Dungeon includes several rooms and roomtypes which will be spawned randomly at the beginning of a Level. 
  * The number of rooms and monsters which are spawned is relative to the difficulty level which is raised after each boss.
  * 
- * @author mourthag
+ * @author mourthag,t4b
  */
 public class Dungeon extends World
 {
-
     public int difficulty = 26;
-    public static final int frameWidth = 1131;
-    public static final int frameHeight = 725;
+    public static final int frameWidth = Integer.valueOf(JOptionPane.showInputDialog("Width","1131"));      //tmp no control for invalid values
+    public static final int frameHeight = Integer.valueOf(JOptionPane.showInputDialog("Height","725"));     //tmp no control for invalid values
     public static final int tileCountX = 51; // frameWidth % tileCountX should be 0, and frameWidth/tileCountX=frameHeight/tileCountY
     public static final int tileCountY = 34; // frameHight % tileCountY should be 0
     public static final int tileWidth = frameWidth/tileCountX;
@@ -40,8 +40,8 @@ public class Dungeon extends World
 
         //tmp playground
         //addObject(new Wall(), 45,45);
-        addObject(new Tile(), 70,45);
-        addObject(new Player(), 70, 70);
+        //addObject(new Tile(), 70,45);
+        addObject(new Player(), 150, 150);
 
         spawnRoom( 100,100,10,10);
     }
@@ -55,23 +55,23 @@ public class Dungeon extends World
         //coordinates for the opposite start
         int opX = x + (width - 1) * tileWidth;
         int opY = y + (height - 1) * tileHeight ;
-        
+
         //inner coordinates for the floor
         int innerWidth = width - 2;
         int innerHeight = height - 2;
         int innerX = x + tileWidth;
         int innerY = y + tileHeight;
-        
+
         //outer Boarders
         createWallLine( x, y, true, width);                             //top
         createWallLine( x, y + tileHeight, false, height - 1);          //left
         createWallLine( innerX, opY, true, width - 1);                  //bottom
         createWallLine( opX, innerY, false, height - 2);                 //right
-        
+
         //Fill with Floor
         for(int k = 0; k<innerHeight; k++)
         {
-            createFloorLine(innerX , innerY + k*tileWidth, true, innerWidth);
+            createFloorLine(innerX , innerY + k*tileHeight, true, innerWidth);
         }
     }
 
